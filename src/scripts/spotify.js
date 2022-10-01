af5032b1bbf14558162cdf21b1864c006ae3940b
Riley Grant <grantrileyh@gmail.com>
2022-10-01 13:20:24 -0500

    Update urls for github pages deployment

--- src/scripts/spotify.js
+++ src/scripts/spotify.js
@@ -1,5 +1,6 @@
 const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = "http://localhost:3000";
+// const redirectUri = "http://localhost:3000";
+const redirectUri = "https://rileyhgrant.github.io/spotify-album-art-player-client/";
 export const CLIENT_ID = "47a6dc56f30f467090615ebd2371aa1f";
 
 const scopes = ["streaming", "user-read-playback-state"];
