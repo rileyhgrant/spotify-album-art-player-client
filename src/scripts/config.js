da0349ce9cc6d0f95033dac79741c1224f2d3b43
Riley Grant <grantrileyh@gmail.com>
2023-03-04 10:02:22 -0600

    Update Spotify client public key for new account

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 export const config = {
   AUTH_ENDPOINT: "https://accounts.spotify.com/authorize",
-  CLIENT_ID: "47a6dc56f30f467090615ebd2371aa1f",
+  CLIENT_ID: "cef624f457c34714ae06c44372304f51",
   REDIRECT_URI: isProd
     ? "https://rileyhgrant.github.io/spotify-album-art-player-client/"
     : "http://localhost:3000",
